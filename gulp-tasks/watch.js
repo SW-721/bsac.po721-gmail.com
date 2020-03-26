04d0ef757a20db062413cb07b7365188a55bceef
Alexandr <bsac.po721@gmail.com>
2020-03-26 13:42:37 +0300

    hotfix watch.js

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -15,10 +15,11 @@ module.exports = function (gulp, options, plugins) {
 				options.path.watch.sass, 
 				'!src/assets/css/critical/critical.scss',
 				'!src/assets/css/common/_bootstrap-opts.sass',
+				'!src/assets/css/common/_settings.scss',
 			], gulp.series(gulp.parallel('sass-styles')))
 		
 		//~~~~~~~~~~~ для обновления critical после обновления какого-то scss, добавтиь в массив watch путь к файлу и сверху добавить его но с !
-		gulp.watch(['./src/**/_bootstrap-opts.{scss,sass}'], gulp.series('sass-styles', 'sass-crit', 'pug'))
+		gulp.watch(['./src/**/_bootstrap-opts.{scss,sass}', './src/**/_settings.scss'], gulp.series('sass-styles', 'sass-crit', 'pug'))
 		gulp.watch(['./src/**/critical.{scss,sass}'], gulp.series('sass-crit', 'pug'))
 
 		// PUG
